2c5d35a6c7a036512714a93305a1766947632fee
layne913 <enjoylaynelife@163.com>
2016-03-21 11:13:04 +0800

    reset

--- modules/users/client/controllers/individual.client.controller.js
+++ modules/users/client/controllers/individual.client.controller.js
@@ -5,14 +5,20 @@
     .module('users')
     .controller('IndividualController', IndividualController);
 
-  IndividualController.$inject = ['$scope', 'userResolve'];
+  IndividualController.$inject = ['$scope', 'userResolve', '$resource'];
 
-  function IndividualController($scope, user) {
+  function IndividualController($scope, user,$resource) {
     var vm = this;
 
     // Individual controller logic
     // ...
     $scope.user = user;
+
+    $scope.addFriend= function(){
+
+        $resource('/api/users/friends', {userId: user._id}, {'update': {method:'PUT'}});
+    };
+
     init();
 
     function init() {
