7e5ae1c5bb33de1c9d8c21eae72f957b7227e005
CyrusCai <Cyrus_Cai@epam.com>
2016-03-23 14:10:56 +0800

    change domain of ngrok to goTogether

--- config/lib/ngrok.js
+++ config/lib/ngrok.js
@@ -4,11 +4,19 @@ var ngrok = require('ngrok');
 
 module.exports.initConnection = function initConnection(){
   console.log("gnrok.initConnection is called");
-  ngrok.connect(3000,function (err, url) {
+  ngrok.connect({
+    proto: 'http', // http|tcp|tls
+    addr: 3000, // port or network address
+    subdomain: 'gotogether' // reserved tunnel name https://alex.ngrok.io,
+  },function (err, url) {
     console.log("Ngrok start to listen.");
     console.log("url:"+url);
     if(err){
       console.log(err);
     }
   });
+};
+
+module.exports.closeConnection = function closeConnection(){
+  ngrok.disconnect();
 };
