0c6947aee70a77014111bcd8fb0d6e4df76ca7f8
CyrusCai <Cyrus_Cai@epam.com>
2016-03-23 14:58:17 +0800

    change some conflict

--- config/lib/ngrok.js
+++ config/lib/ngrok.js
@@ -4,18 +4,27 @@ var ngrok = require('ngrok');
 
 module.exports.initConnection = function initConnection(){
   console.log("gnrok.initConnection is called");
-  ngrok.connect({
-    proto: 'http', // http|tcp|tls
-    addr: 3000, // port or network address
-    subdomain: 'gotogether' // reserved tunnel name https://alex.ngrok.io,
-  },function (err, url) {
+   ngrok.connect(9090, function (err, url) {
     console.log("Ngrok start to listen.");
     console.log("url:"+url);
     if(err){
+      console.log("err: ");
       console.log(err);
-    }
+      }
   });
-};
+  // ngrok.connect({
+  //   proto: 'http', // http|tcp|tls
+  //   addr: 3000, // port or network address
+  //   subdomain: 'gotogether', // reserved tunnel name https://alex.ngrok.io,
+  // }, function (err, url) {
+  //   console.log("Ngrok start to listen.");
+  //   console.log("url:"+url);
+  //   if(err){
+  //     console.log("err: ");
+  //     console.log(err);
+  //     }
+  // });
+}
 
 module.exports.closeConnection = function closeConnection(){
   ngrok.disconnect();
--- modules/users/server/routes/users.server.routes.js
+++ modules/users/server/routes/users.server.routes.js
@@ -13,13 +13,7 @@ module.exports = function (app) {
 
   app.route('/api/users/search/:email').get(users.search);
   app.route('/api/users/friends/:userId').get(users.finduser);
-<<<<<<< HEAD
-  app.route('/api/users/friends').put(users.addfriend);
-  //add by fiona 0321
-  // app.route('/api/users/activities/:activityId').get(users.findactivity);
-=======
   app.route('/api/users/friends').put(users.updateFriends);
->>>>>>> b2778eb670320e442033982b5c6700f580899a5d
   // Finish by binding the user middleware
   app.param('userId', users.userByID);
 };
